4905d91695a80f15d0d794823b28b57c793d9ceb
puszkarek <guipuszkarek@gmail.com>
2022-09-01 19:43:00 -0300

    ⚰️ remove dwiki

--- src/services/utils.service.ts
+++ src/services/utils.service.ts
@@ -7,8 +7,6 @@ import { createReplyMessage, createStickerMessage, getCommandList } from '~/help
 import { AsyncServiceMethod, MessageResponse, ServiceMethod } from '~/interfaces';
 import { NEEDS_MEDIA_MESSAGE, SEARCH_ERROR_MESSAGE } from '~/messages';
 
-import dwiki from '../../libs/desciclopedia/dist';
-
 // TODO: needs that? if no delete
 const uaOverride =
   'WhatsApp/2.16.352 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Safari/605.1.15';
@@ -24,23 +22,6 @@ export const generateUtilsService = (): {
 
   // * Return the methods
   return {
-    // TODO: move to a regional service
-    /** Search terms on the Desciclopedia */
-    dwiki: async ({ parsedMessageText }): Promise<MessageResponse> => {
-      try {
-        const searchResults = await dwiki.search(parsedMessageText);
-
-        const page = await dwiki.page(searchResults.results[0].pageid);
-        const content = await page.content({
-          redirect: false,
-        });
-
-        return createReplyMessage(content.replaceAll('\n', '\n\n'));
-      } catch {
-        return createReplyMessage(SEARCH_ERROR_MESSAGE);
-      }
-    },
-
     /** Show a list of available command */
     help: (): MessageResponse => createReplyMessage(displayedCommandList),
 
